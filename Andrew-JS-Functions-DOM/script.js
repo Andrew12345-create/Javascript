5355aef624f983afba6000d252a405fa9ca77885
Andrew Mwangi <andrewmunamwangi@gmail.com>
2025-02-28 08:58:44 +0300

    123

---
+++ Andrew-JS-Functions-DOM/script.js
@@ -0,0 +1,20 @@
+/*function greet() {
+    console.log('Hello Andrew Muna Mwangi!');
+}
+greet();*/
+
+
+function greet(name) {
+    console.log('Hello ' + name);
+}
+greet('Andrew');
+
+
+function add(a, b) {
+    return a + b;
+}
+let age = "I am age";
+let space = " "
+let sum = add(10, 1);a
+
+console.log(age + space + sum);
